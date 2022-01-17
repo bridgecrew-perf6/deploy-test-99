d355b5879059efb97f6baa730f04f9ff49fedd14
casey <cassandra.d.mclellan@gmail.com>
2022-01-17 18:46:36 +0000

    CHORE: made db script to create table

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,6 +1,6 @@
-import { Pool } from 'pg';
+import pg from 'pg';
 
-const pool = new Pool({
+const pool = new pg.Pool({
     user: process.env.PGUSER,
     host: process.env.PGHOST,
     database: process.env.PGDATABASE,
