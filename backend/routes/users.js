80011c25b54b10efa6694e4bd73b92a8cf5eec87
casey <cassandra.d.mclellan@gmail.com>
2022-01-17 19:01:58 +0000

    CHORE: made populate table script and route to get all users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,13 @@
 import express from "express";
 const router = express.Router();
 
+import query from "../db/index.js";
+
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.json({ message: "I wish we had some information to give you ☹️" });
+router.get("/", async function (req, res, next) {
+  const data = await query(`SELECT * FROM users;`);
+
+  res.json({ message: "here are all the users", payload: data.rows });
 });
 
 export default router;
